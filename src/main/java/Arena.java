7dff7fb775088dc00b90aba954be6603b370fef2
Heidi Bucio Martinez <buciomartinezh@xavier.edu>
2026-04-20 22:33:21 -0400

    Add to main, change Arean and RandomEnemy

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -1,38 +1,32 @@
 public class Arena {
-    public static void Arena(){
-        Character p1;
-        Character p2;
+
+    private Character p1;
+    private Character p2;
+
+    public Arena(Character c1, Character c2) {
+        if (c1.getSpeed() >= c2.getSpeed()) {
+            p1 = c1;
+            p2 = c2;
+        } else {
+            p1 = c2;
+            p2 = c1;
+        }
+    }
+
+    public void startFight() {
         boolean fightOver = false;
-        Weapon sword = new Weapon(5, 0);
-        Weapon axe = new Weapon (3, 0);
-        Armor basicArmor = new Armor(3, 2);
-        Character contender1 = new SampleCharacter(sword, basicArmor, "John");
-        Character contender2 = new RandomEnemy(axe, basicArmor, "Bob");
 
-        if(contender1.getSpeed() >= contender2.getSpeed()){
-            p1 = contender1;
-            p2 = contender2;
-        }else{
-            p1 = contender2;
-            p2 = contender1;
-        }
-
-        int hp1;
-        int hp2;
-
-        while (!fightOver){
+        while (!fightOver) {
             p2.takeDamage(p1.dealDamage());
+            if (p2.getHP() <= 0) break;
+
             p1.takeDamage(p2.dealDamage());
-            hp1 = p1.getHP();
-            hp2 = p2.getHP();
-            if (hp1 <= 0 || hp2 <= 0){
-                fightOver = true;
-            }
+            if (p1.getHP() <= 0) break;
         }
-        if (p1.getHP() <= 0){
+
+        if (p1.getHP() <= 0) {
             System.out.println(p2.getName() + " has won the battle!");
-        }
-        if (p2.getHP() <= 0){
+        } else {
             System.out.println(p1.getName() + " has won the battle!");
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,16 @@ public class Main {
     public static void main(String[] args){
         Character Mage = new Character(10, 10, new Weapon(2,1),new Armor(2,2),"Joseph",1,2);
         BallerBrawler e = new BallerBrawler("joe");
+
+        System.out.println("Program started");
+
+        Weapon sword = new Weapon(5, 0);
+        Armor armor = new Armor(3, 2);
+
+        Character c1 = new SampleCharacter(sword, armor, "John");
+        Character c2 = new SampleCharacter(sword, armor, "Bob");
+
+        Arena arena = new Arena(c1, c2);
+        arena.startFight();
     }
 }
